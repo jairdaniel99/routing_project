88bfaff962d3cb123f0f3bbf63db5f27aad2265b
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-16 19:49:07 -0600

    set up rerouting (for project)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentsComponent } from './students/students.component';
-import { TeachersComponent } from './teachers/teachers.component';
-import { HomeComponent } from './home/home.component';
+import { Cmp1Component } from './cmp1/cmp1.component';
+import { Cmp2Component } from './cmp2/cmp2.component';
+import { Cmp3Component } from './cmp3/cmp3.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 // We can define paths/urls to components in the routes array.
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
-  { path: 'students', component: StudentsComponent },
-  { path: 'teachers', component: TeachersComponent },
+  { path: 'cmp1', component: Cmp1Component },
+  { path: 'cmp2', component: Cmp2Component },
+  { path: 'cmp3', component: Cmp3Component },
+  { path: '**', component: NotFoundComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { TeachersComponent } from './teachers/teachers.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Cmp1Component } from './cmp1/cmp1.component';
+import { Cmp2Component } from './cmp2/cmp2.component';
+import { Cmp3Component } from './cmp3/cmp3.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentsComponent,
     TeachersComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    Cmp1Component,
+    Cmp2Component,
+    Cmp3Component,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
